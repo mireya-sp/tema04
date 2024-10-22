7a6bdab2eb2cfd1fcdf36d2826ad27c1df3f242b
Mireya Serrano <mireyaserrano181105@gmail.com>
2024-10-22 15:55:19 +0200

    Ej1 metodos

---
+++ src/main/java/com/mireyaserrano/tema04/Ejercicio1.java
@@ -0,0 +1,11 @@
+package com.mireyaserrano.tema04;
+
+public class Ejercicio1 {
+    public static int sumar(int x, int y){
+        return x + y;
+    }
+    public static void main(String args[]){
+        int resultado = sumar(5,3);
+        System.out.println(resultado);
+    }
+}
