e37d9bb5cd84d3bdd8beb7d99a96d6038df951a9
Mireya Serrano <mireyaserrano181105@gmail.com>
2024-10-23 16:50:09 +0200

    ej 7 métodos

---
+++ src/main/java/com/mireyaserrano/tema04/Ejercicio7.java
@@ -0,0 +1,28 @@
+package com.mireyaserrano.tema04;
+
+import java.util.Scanner;
+
+public class Ejercicio7 {
+    private static int factorial(int num) {
+        int resultado = 1;
+        for (int i = 1; i <= num; i++){
+            resultado = i * resultado;
+        }
+        return resultado;
+    }
+
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        boolean valido;
+        int a;
+        do {
+            System.out.println("Introduce un número y imprimo su factorial");
+            a = Integer.parseInt(scanner.nextLine());
+            valido = a>0;
+            if (a <= 0) {
+                System.err.println("Introduce un número positivo");
+            }
+        }while (!valido);
+        System.out.println("El fatorial del número introducido es " + factorial(a));
+    }
+}
