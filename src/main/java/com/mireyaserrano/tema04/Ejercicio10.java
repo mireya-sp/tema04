253086160c40f8c6c96c4c19bf3466d5bce845b4
Mireya Serrano <mireyaserrano181105@gmail.com>
2024-10-24 13:12:17 +0200

    ej 10 métodos

--- src/main/java/com/mireyaserrano/tema04/Ejercicio10.java
+++ src/main/java/com/mireyaserrano/tema04/Ejercicio10.java
@@ -1,4 +1,24 @@
 package com.mireyaserrano.tema04;
 
+import java.util.Scanner;
+
 public class Ejercicio10 {
+    public static void tablaMulti(int num){
+        for (int i = 1; i <= 10; i++){
+            int resultado = num * i;
+            System.out.println(num + " x " + i + " = " + resultado);
+        }
+    }
+
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce el número de la tabla de multiplicar que quieres imprimir");
+        int x = Integer.parseInt(scanner.nextLine());
+        while (x >= 0){
+            tablaMulti(x);
+            System.out.println("Introduce el número de la tabla de multiplicar que quieres imprimir");
+            x = Integer.parseInt(scanner.nextLine());
+        }
+        scanner.close();
+    }
 }
