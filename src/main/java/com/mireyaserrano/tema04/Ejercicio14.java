61ed02301e9e5b56785e299b42283a523e72b0f1
Mireya Serrano <mireyaserrano181105@gmail.com>
2024-10-24 20:12:11 +0200

    ej 14 métodos

---
+++ src/main/java/com/mireyaserrano/tema04/Ejercicio14.java
@@ -0,0 +1,26 @@
+package com.mireyaserrano.tema04;
+
+import java.util.Scanner;
+
+public class Ejercicio14 {
+    public static Float resultado(int n, int m){
+        int resta = n - m;
+        int factN = Ejercicio7.factorial(n);
+        int factM = Ejercicio7.factorial(m);
+        int factNM = Ejercicio7.factorial(resta);
+        float resultado = factN / (factM * factNM);
+        return resultado;
+    }
+
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int factN, factM, factNM;
+        System.out.println("Introduce n");
+        int n = Integer.parseInt(scanner.nextLine());
+        System.out.println("Introduce m");
+        int m = Integer.parseInt(scanner.nextLine());
+        scanner.close();
+
+        System.out.println("El resultado del número combinatorio es " + resultado(n,m));
+    }
+}
