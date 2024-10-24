5c8a8e870f06820d08a488aa8059107baba97605
Mireya Serrano <mireyaserrano181105@gmail.com>
2024-10-24 18:30:32 +0200

    ej 11 métodos

---
+++ src/main/java/com/mireyaserrano/tema04/Ejercicio11.java
@@ -0,0 +1,41 @@
+package com.mireyaserrano.tema04;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ejercicio11 {
+    private static Integer aleatorio(int max, int min){
+        Random aleatorio = new Random();
+        int goles;
+        do {
+            goles = aleatorio.nextInt(max);
+        }while (goles < min);
+        return goles;
+    }
+
+    public static String resultado(int maxGoles, int minGoles){
+        String total;
+        int equipoLocal = aleatorio(maxGoles, minGoles);
+        int equipoVisitante = aleatorio(maxGoles, minGoles);
+        if (equipoLocal == equipoVisitante){
+            total = "x";
+        } else if (equipoLocal > equipoVisitante) {
+            total = "1";
+        }else {
+            total = "2";
+        }
+        return total;
+    }
+
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce el valor mínimo que pueden tener los goles");
+        int minGoles = Integer.parseInt(scanner.nextLine());
+        System.out.println("Introduce el valor máximo que pueden tener los goles");
+        int maxGoles = Integer.parseInt(scanner.nextLine());
+        scanner.close();
+        for (int i = 1; i <= 15; i++){
+            System.out.printf("Partido" + i + "%8s\n", resultado(maxGoles, minGoles));
+        }
+    }
+}
