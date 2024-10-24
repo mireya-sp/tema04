e1c38938659018d7fe673fada3b7eb426d9e58ac
Mireya Serrano <mireyaserrano181105@gmail.com>
2024-10-24 12:51:48 +0200

    ej 9 métodos

---
+++ src/main/java/com/mireyaserrano/tema04/Ejercicio9.java
@@ -0,0 +1,20 @@
+package com.mireyaserrano.tema04;
+
+import java.util.Scanner;
+
+public class Ejercicio9 {
+    private static Integer sumatorio(int num){
+        int resultado = 0;
+        for (int i = num; i > 0; i--){
+            resultado += i;
+        }
+        return resultado;
+    }
+
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce un número y se calculará el sumatorio de los números anteriores");
+        int x = Integer.parseInt(scanner.nextLine());
+        System.out.println("El sumatorio de " + x + " hasta el 1 es " + sumatorio(x));
+    }
+}
