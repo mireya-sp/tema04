5c40176fc656c652e524f88bff5c333fed902568
Mireya Serrano <mireyaserrano181105@gmail.com>
2024-10-24 12:39:22 +0200

    ej 8 métodos

---
+++ src/main/java/com/mireyaserrano/tema04/Ejercicio8.java
@@ -0,0 +1,36 @@
+package com.mireyaserrano.tema04;
+
+import java.util.Scanner;
+
+public class Ejercicio8 {
+    private static boolean capicua(String cadena){
+        boolean valido;
+        int n1, n2, n3, n4;
+        n1 = cadena.charAt(0);
+        n2 = cadena.charAt(1);
+        n3 = cadena.charAt(2);
+        n4 = cadena.charAt(3);
+        valido = (n1 == n4) && (n2 == n3);
+        return valido;
+    }
+
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        String numeros;
+        boolean correcto;
+        do{
+            System.out.println("Introduce una cadena de CUATRO NÚMEROS");
+            numeros = scanner.nextLine();
+            correcto = (numeros.length() == 4);
+            if (!correcto){
+                System.err.println("LA CADENA DEBE SER DE CUATRO CARACTERES");
+            }
+        }while (!correcto);
+
+        if (capicua(numeros) == true){
+            System.out.println("El número es capicua");
+        }else {
+            System.out.println("El número no es capicua");
+        }
+    }
+}
